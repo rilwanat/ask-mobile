6f5d8d43829d2ddb24dc98374ecfd71eb5082b6a
rilwanat <49300698+rilwanat@users.noreply.github.com>
2025-06-02 00:44:13 +0100

    update
    - all instance of error: Attention
    - kyc: KYC
    - on DNQ tap expandable info explaining DNQ
    - boost DNQ option
    - bank/donations/crypto data bug resolved pls confirm
    - forgot password
    - help/contacts page

--- lib/app/modules/home/views/faq_view.dart
+++ lib/app/modules/home/views/faq_view.dart
@@ -8,6 +8,8 @@ import '../../../../global/widgets/app_bar.dart';
 import '../../../../global/widgets/navbar.dart';
 import '../controllers/home_controller.dart';
 
+import 'package:url_launcher/url_launcher.dart';
+
 class FaqView extends GetView<HomeController> {
   const FaqView({super.key});
   @override
@@ -26,7 +28,7 @@ class FaqView extends GetView<HomeController> {
         iconTheme: const IconThemeData(color: AppColors.white),
         title: const
         Text(
-          'A.S.K- FAQs',
+          'A.S.K - Contact',
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.w600,
@@ -64,10 +66,183 @@ class FaqView extends GetView<HomeController> {
                                 child: Column(
                                   children: [
 
-                                    FaqItem(
-                                      question: "How do I ...",
-                                      answer: "To ..",
+                                    // FaqItem(
+                                    //   question: "How do I ...",
+                                    //   answer: "To ..",
+                                    // ),
+
+                                    Container(
+                                      margin: const EdgeInsets.only(bottom: 16),
+                                      width: ScreenSize.width(context),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(16.0),
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            const Row(
+                                              children: [
+                                                Icon(Icons.phone),
+                                                SizedBox(width: 8),
+                                                Text(
+                                                  "Phone Number",
+                                                  style: TextStyle(
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: AppColors.askBlue,
+                                                    fontFamily: "LatoRegular",
+
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            const SizedBox(height: 8),
+                                            GestureDetector(
+                                              onTap: () => _launchUrl("tel:+2349122090051"),
+                                              child: const Text(
+                                                "+234 912 2090 051",
+                                                style: TextStyle(
+                                                  fontSize: 14,
+                                                  height: 1.5,
+                                                  fontFamily: "LatoRegular",
+                                                ),
+                                              ),
+                                            ),
+                                            const SizedBox(height: 8),
+                                            GestureDetector(
+                                              onTap: () => _launchUrl("tel:+447864869571"),
+                                              child: const Text(
+                                                "+44 786 4869 571",
+                                                style: TextStyle(
+                                                  fontSize: 14,
+                                                  height: 1.5,
+                                                  fontFamily: "LatoRegular",
+                                                ),
+                                              ),
+                                            ),
+                                            const SizedBox(height: 8),
+                                            GestureDetector(
+                                              onTap: () async {
+                                                const whatsappUrl = "https://wa.me/2349051047138";
+                                                const fallbackUrl = "https://api.whatsapp.com/send?phone=2349051047138";
+
+                                                if (await canLaunchUrl(Uri.parse(whatsappUrl))) {
+                                                  await launchUrl(
+                                                    Uri.parse(whatsappUrl),
+                                                    mode: LaunchMode.externalApplication, // This is important
+                                                  );
+                                                } else if (await canLaunchUrl(Uri.parse(fallbackUrl))) {
+                                                  await launchUrl(Uri.parse(fallbackUrl), mode: LaunchMode.externalApplication);
+                                                } else {
+                                                  // Fallback or show error
+                                                  ScaffoldMessenger.of(context).showSnackBar(
+                                                    SnackBar(content: Text("Could not launch WhatsApp")),
+                                                  );
+                                                }
+                                              },
+                                              child: const Text(
+                                                "+234 905 1047 138",
+                                                style: TextStyle(
+                                                  fontSize: 14,
+                                                  height: 1.5,
+                                                  fontFamily: "LatoRegular",
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
                                     ),
+
+                                    Container(
+                                      margin: const EdgeInsets.only(bottom: 16),
+                                      width: ScreenSize.width(context),
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(16.0),
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            const Row(
+                                              children: [
+                                                Icon(Icons.email),
+                                                SizedBox(width: 8),
+                                                Text(
+                                                  "Email Address",
+                                                  style: TextStyle(
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: AppColors.askBlue,
+                                                    fontFamily: "LatoRegular",
+
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            const SizedBox(height: 8),
+                                            GestureDetector(
+                                              onTap: () async {
+                                                const url = "mailto:info@askfoundations.org";
+                                                if (await canLaunchUrl(Uri.parse(url))) {
+                                                  await launchUrl(
+                                                    Uri.parse(url),
+                                                    mode: LaunchMode.externalApplication,
+                                                  );
+                                                } else {
+                                                  ScaffoldMessenger.of(context).showSnackBar(
+                                                    SnackBar(content: Text("Could not launch email client")),
+                                                  );
+                                                }
+                                              },
+                                              child: const Text(
+                                                "info@askfoundations.org",
+                                                style: TextStyle(
+                                                  fontSize: 14,
+                                                  height: 1.5,
+                                                  fontFamily: "LatoRegular",
+                                                ),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+
+                                    Container(
+                                      margin: const EdgeInsets.only(bottom: 16),
+                                      width: ScreenSize.width(context),
+                                      child: const Padding(
+                                        padding: EdgeInsets.all(16.0),
+                                        child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Row(
+                                              children: [
+                                                Icon(Icons.place),
+                                                SizedBox(width: 8),
+                                                Text(
+                                                  "Address",
+                                                  style: TextStyle(
+                                                    fontSize: 16,
+                                                    fontWeight: FontWeight.bold,
+                                                    color: AppColors.askBlue,
+                                                    fontFamily: "LatoRegular",
+
+                                                  ),
+                                                ),
+                                              ],
+                                            ),
+                                            SizedBox(height: 8),
+                                            Text(
+                                              "69 Avenue Road, \nBexleyheath Kent DA7 4EQ, \nLondon.",
+                                              style: TextStyle(
+                                                fontSize: 14,
+                                                height: 1.5,
+                                                fontFamily: "LatoRegular",
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    )
 
 
                                   ],
@@ -135,5 +310,13 @@ class FaqItem extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+
+
+Future<void> _launchUrl(String url) async {
+  if (await canLaunchUrl(Uri.parse(url))) {
+    await launchUrl(Uri.parse(url));
   }
 }
