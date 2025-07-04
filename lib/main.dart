56e257d2ab6929896813862ddb4b2a2b1f979ac3
rilwanat <49300698+rilwanat@users.noreply.github.com>
2025-07-04 12:30:55 +0100

    update on deeplinks

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,31 @@
+import 'dart:async';
 import 'package:ask_mobile/app/modules/home/views/requests_view.dart';
 import 'package:flutter/material.dart';
-
 import 'package:get/get.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
-import 'package:firebase_dynamic_links/firebase_dynamic_links.dart';
-
+import 'package:app_links/app_links.dart';
 import 'app/data/storage/cached_data.dart';
 import 'app/modules/home/bindings/home_binding.dart';
 import 'app/routes/app_pages.dart';
-
 import 'package:firebase_core/firebase_core.dart';
 
 final CachedData _cachedData = CachedData();
 String? userType;
-
+late AppLinks _appLinks;
+StreamSubscription<Uri>? _linkSubscription;
 
 void main() async {
-
   WidgetsFlutterBinding.ensureInitialized();
   await MobileAds.instance.initialize();
-
   await Firebase.initializeApp();
 
   userType = await _cachedData.getUserType();
 
-// Initialize deep linking
-  await initDynamicLinks();
+  // Initialize AppLinks instance
+  _appLinks = AppLinks();
 
+  // Initialize deep linking
+  initDeepLinking();
 
   runApp(
     GetMaterialApp(
@@ -38,35 +37,40 @@ void main() async {
   );
 }
 
-Future<void> initDynamicLinks() async {
-  // Handle links when app is opened from terminated state
-  final PendingDynamicLinkData? initialLink =
-  await FirebaseDynamicLinks.instance.getInitialLink();
+void initDeepLinking() {
+  // Get the initial link if app was launched from one
+  _appLinks.getInitialLink().then((Uri? uri) {
+    debugPrint('Initial URI: $uri');
+    if (uri != null) {
+      _handleDeepLink(uri);
+    }
+  });
 
-  if (initialLink != null) {
-    _handleDynamicLink(initialLink);
-  }
-
-  // Handle links when app is in foreground/background
-  FirebaseDynamicLinks.instance.onLink.listen((dynamicLinkData) {
-    _handleDynamicLink(dynamicLinkData);
-  }).onError((error) {
-    print('Dynamic Link Failed: ${error.message}');
+  // Listen for incoming links
+  _linkSubscription = _appLinks.uriLinkStream.listen((Uri uri) {
+    debugPrint('Incoming deep link: $uri');
+    _handleDeepLink(uri);
+  }, onError: (error) {
+    debugPrint('Deep link error: $error');
   });
 }
 
-void _handleDynamicLink(PendingDynamicLinkData dynamicLinkData) {
-  final Uri uri = dynamicLinkData.link;
+void _handleDeepLink(Uri uri) {
+  debugPrint('Handling deep link: $uri');
 
-  if (uri.path.startsWith('/help-request/')) {
-    String requestId = uri.pathSegments[1];
+  if (uri.pathSegments.length >= 2 &&
+      uri.pathSegments.first == 'help-request') {
+    final requestId = uri.pathSegments[1];
+    debugPrint('Navigating to request: $requestId');
 
     Get.to(
           () => RequestsView(),
       transition: Transition.fadeIn,
-      duration: Duration(milliseconds: 500),
+      duration: const Duration(milliseconds: 500),
       binding: HomeBinding(),
       arguments: {'requestId': requestId},
     );
+  } else {
+    debugPrint('Unhandled deep link path: ${uri.path}');
   }
 }
